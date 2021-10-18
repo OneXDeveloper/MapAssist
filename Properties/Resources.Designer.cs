0acd6a0159989143a1aa9495650179ea2c59c336
illoan <illoprogs@gmail.com>
2021-10-18 20:27:33 +0200

    Added SuperChest

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -23,7 +23,7 @@ namespace D2RAssist.Properties
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources
     {
-
+        
         private static global::System.Resources.ResourceManager resourceMan;
 
         private static global::System.Globalization.CultureInfo resourceCulture;
