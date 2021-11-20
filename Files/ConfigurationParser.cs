e3286d1b1fd9d1ed2b9a1cbac642ffba3d32c20b
Charleon <Charleon@gmail.com>
2021-11-20 01:37:13 +0100

    Change ConfigurationParser to look for a nonstatic JSON file

--- Files/ConfigurationParser.cs
+++ Files/ConfigurationParser.cs
@@ -10,7 +10,23 @@ namespace MapAssist.Files
     {
         public T ParseConfiguration()
         {
-            var fileName = "MapAssistConfiguration.json";
+            var fileName = "";
+            
+            foreach(var fileNameInDirectory in System.IO.Directory.EnumerateFiles(System.IO.Directory.GetCurrentDirectory())) 
+            {
+                if(fileNameInDirectory.Contains("json"))
+                {
+                    fileName = fileNameInDirectory;
+                    Debug.WriteLine($"Configuration file {fileNameInDirectory} found!");
+                    break;
+                }
+            }
+
+            if(fileName.Length < 1)
+            {
+                throw new Exception("Configuration JSON file not found!");
+            }
+
             var fileManager = new FileManager(fileName);
             var JSONString = fileManager.ReadFile();
             if (JSONString.Length <= 0)
